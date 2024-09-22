78c20a7556b98e70b939b540cc1ceab1c10aa2b7
Arup 53 <arupkumarhowlader12@gmail.com>
2024-09-23 01:49:40 +0600

    2nd commit

---
+++ js/functionalityMain.js
@@ -0,0 +1,86 @@
+// Reuseable component
+
+function getId(id) {
+  return document.getElementById(id);
+}
+
+function getInnerText(id) {
+  return document.getElementById(id).innerText;
+}
+
+function getValue(id) {
+  return document.getElementById(id).value;
+}
+
+function getClassList(id) {
+  return document.getElementById(id).classList;
+}
+
+// toggle functionality
+
+const donate = getId("donate");
+const history = getId("history");
+
+history.addEventListener("click", function () {
+  const donateContent = getClassList("donateContent");
+  const historyContent = getClassList("historyContent");
+
+  donate.classList.remove("text-black");
+  donate.classList.add("text-gray-500");
+  history.classList.remove("text-gray-500");
+  history.classList.add("text-black");
+  history.classList.add("bg-[#b4f461]");
+  donate.classList.remove("bg-[#b4f461]");
+
+  historyContent.remove("hidden");
+  donateContent.add("hidden");
+});
+
+donate.addEventListener("click", function () {
+  const donateContent = getClassList("donateContent");
+  const historyContent = getClassList("historyContent");
+
+  donate.classList.remove("text-gray-500");
+  donate.classList.add("text-black");
+  history.classList.remove("text-black");
+  history.classList.add("text-gray-500");
+  donate.classList.add("bg-[#b4f461]");
+  history.classList.remove("bg-[#b4f461]");
+
+  donateContent.remove("hidden");
+  historyContent.add("hidden");
+});
+
+// // toggle functionality ends
+
+// Donate card -1 functionality
+const card1btn = getId("card-1-btn");
+
+card1btn.addEventListener("click", function () {
+  const userBalanceValue = parseFloat(getInnerText("account-balance"));
+  const card1Inuput = parseFloat(getValue("card-1-input"));
+
+  if (isNaN(card1Inuput)) {
+    alert("invalid input");
+    return;
+  }
+
+  const totalBalance = userBalanceValue - card1Inuput;
+
+  document.getElementById("account-balance").innerText = totalBalance;
+
+  console.log({ userBalanceValue, card1Inuput, totalBalance });
+
+  const item = document.createElement("div");
+  item.className = "border-2 rounded-md p-5 mb-5";
+
+  item.innerHTML = ` 
+    <p class= "text-base font-extrabold mb-2">${card1Inuput} Taka is Donated for flood-2024 at Nohakhali, Bangladesh </p>
+    <p class= "text-xs font-normal mb-2">${new Date().toString()}</p>
+  `;
+
+  const historyContainer = getId("historyContent");
+  historyContainer.insertBefore(item, historyContainer.firstChild);
+});
+
+// // Donate card -1 functionality ends
